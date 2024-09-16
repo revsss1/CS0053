f0d4fa635a6fac7e675348a985199908206a1eea
KRevilla <115548960+revsss1@users.noreply.github.com>
2024-09-17 07:40:09 +0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -7,29 +7,30 @@ public class calculator implements ActionListener {
     JFrame frame;
     JTextField textField;
     JButton[] numbers = new JButton[10];
-    JButton[] methods = new JButton[5];
-    JButton add, sub, mul, div, equ;
-    JPanel panel, panel2;
+    JButton[] methods = new JButton[6];
+    JButton add, sub, mul, div, equ, del;
+    JPanel panel;
 
     Color methodColor = new Color(77, 148, 255);
     Color backgroundColor = new Color(245, 245, 245);
 
-    Font textFieldFont = new Font("Monospaced", Font.BOLD, 30);
+    Font textFieldFont = new Font("SANS_SERIF", Font.BOLD, 80);
     Font buttonFont = new Font("Monospaced", Font.BOLD, 30);
-    double firstNum, secondNum, result;
-    char operation;
+    double firstNum = 0, secondNum = 0, result = 0;
+    char operation = ' ';
+    boolean isOperationClicked = false;
 
     calculator() {
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(450, 625);
+        frame.setSize(350, 550);
         frame.setLayout(null);
 
         frame.getContentPane().setBackground(backgroundColor);
 
         textField = new JTextField();
-        textField.setBounds(65, 25, 300, 100);
-        textField.setHorizontalAlignment(JTextField.CENTER);
+        textField.setBounds(20, 25, 300, 125);
+        textField.setHorizontalAlignment(JTextField.RIGHT);
         textField.setEditable(false);
         textField.setFont(textFieldFont);
 
@@ -38,21 +39,21 @@ public class calculator implements ActionListener {
         mul = new JButton("*");
         div = new JButton("/");
         equ = new JButton("=");
+        del = new JButton("C");
 
         methods[0] = add;
         methods[1] = sub;
         methods[2] = mul;
         methods[3] = div;
         methods[4] = equ;
+        methods[5] = del;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < methods.length; i++) {
             methods[i].addActionListener(this);
             methods[i].setFont(buttonFont);
             methods[i].setFocusable(false);
             methods[i].setBackground(methodColor);
         }
-
-        equ.setBounds(65, 540, 300, 50);
 
         for (int i = 0; i < 10; i++) {
             numbers[i] = new JButton(String.valueOf(i));
@@ -63,35 +64,29 @@ public class calculator implements ActionListener {
         }
 
         panel = new JPanel();
-        panel.setBounds(65, 150, 300, 300);
-        panel.setLayout(new GridLayout(3, 4, 10, 10));
-        panel.setBackground(backgroundColor); // Set the same color as frame
+        panel.setBounds(20, 175, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(backgroundColor);
 
         panel.add(numbers[1]);
         panel.add(numbers[2]);
         panel.add(numbers[3]);
-        panel.add(add);
+        panel.add(del);
         panel.add(numbers[4]);
         panel.add(numbers[5]);
         panel.add(numbers[6]);
-        panel.add(sub);
+        panel.add(add);
         panel.add(numbers[7]);
         panel.add(numbers[8]);
         panel.add(numbers[9]);
+        panel.add(sub);
+        panel.add(numbers[0]);
+        panel.add(equ);
         panel.add(mul);
-
-        panel2 = new JPanel();
-        panel2.setBounds(65, 460, 300, 70);
-        panel2.setLayout(new GridLayout(1, 4, 10, 10));
-        panel2.setBackground(backgroundColor); // Set the same color as frame
-
-        panel2.add(numbers[0]);
-        panel2.add(div);
+        panel.add(div);
 
         frame.add(panel);
-        frame.add(panel2);
         frame.add(textField);
-        frame.add(equ);
         frame.setVisible(true);
     }
 
@@ -99,55 +94,81 @@ public class calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         for (int i = 0; i < 10; i++) {
             if (e.getSource() == numbers[i]) {
+                if (isOperationClicked) {
+                    textField.setText("");
+                    isOperationClicked = false;
+                }
                 textField.setText(textField.getText().concat(String.valueOf(i)));
             }
         }
 
-        if (e.getSource() == add) {
-            firstNum = Double.parseDouble(textField.getText());
-            operation = '+';
-            textField.setText("");
-        }
+        if (e.getSource() == add || e.getSource() == sub || e.getSource() == mul || e.getSource() == div) {
+            if (!textField.getText().equals("")) {
+                secondNum = Double.parseDouble(textField.getText());
 
-        if (e.getSource() == sub) {
-            firstNum = Double.parseDouble(textField.getText());
-            operation = '-';
-            textField.setText("");
-        }
+                if (operation != ' ') {
+                    calculate();
+                } else {
+                    firstNum = secondNum;
+                }
 
-        if (e.getSource() == mul) {
-            firstNum = Double.parseDouble(textField.getText());
-            operation = '*';
-            textField.setText("");
-        }
+                operation = e.getActionCommand().charAt(0);
+                isOperationClicked = true;
+                textField.setText(String.valueOf(firstNum));
 
-        if (e.getSource() == div) {
-            firstNum = Double.parseDouble(textField.getText());
-            operation = '/';
-            textField.setText("");
+                if (result == (int) result) {
+                    textField.setText(String.valueOf((int) result));
+                } else {
+                    textField.setText(String.valueOf(result));
+                }
+            }
         }
 
         if (e.getSource() == equ) {
-            secondNum = Double.parseDouble(textField.getText());
+            if (!textField.getText().equals("")) {
+                secondNum = Double.parseDouble(textField.getText());
+                calculate();
+                operation = ' ';
 
-            switch (operation) {
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-                case '*':
-                    result = firstNum * secondNum;
-                    break;
-                case '/':
-                    result = firstNum / secondNum;
-                    break;
+                if (result == (int) result) {
+                    textField.setText(String.valueOf((int) result));
+                } else {
+                    textField.setText(String.valueOf(result));
+                }
             }
-
-            textField.setText(String.valueOf(result));
-            firstNum = result; 
         }
+
+        if (e.getSource() == del) {
+            textField.setText("");
+            firstNum = 0;
+            secondNum = 0;
+            result = 0;
+            operation = ' ';
+            isOperationClicked = false;
+        }
+    }
+
+    private void calculate() {
+        switch (operation) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                if (secondNum != 0) {
+                    result = firstNum / secondNum;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Cannot divide by zero");
+                    result = 0;
+                }
+                break;
+        }
+        firstNum = result;
     }
 
     public static void main(String[] args) {
