ea96ff114310b6c9103ef10de416f875f95656e7
KRevilla <115548960+revsss1@users.noreply.github.com>
2024-09-17 08:11:08 +0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -4,35 +4,51 @@ import java.awt.event.*;
 
 public class calculator implements ActionListener {
 
+    // initialized everything needed
     JFrame frame;
     JTextField textField;
     JButton[] numbers = new JButton[10];
     JButton[] methods = new JButton[6];
     JButton add, sub, mul, div, equ, del;
     JPanel panel;
+    boolean isOperationClicked = false;
 
     Color methodColor = new Color(77, 148, 255);
     Color backgroundColor = new Color(245, 245, 245);
-
     Font textFieldFont = new Font("SANS_SERIF", Font.BOLD, 80);
     Font buttonFont = new Font("Monospaced", Font.BOLD, 30);
     double firstNum = 0, secondNum = 0, result = 0;
     char operation = ' ';
-    boolean isOperationClicked = false;
 
     calculator() {
+        // create window
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(350, 550);
         frame.setLayout(null);
-
         frame.getContentPane().setBackground(backgroundColor);
 
+        // create a text field
         textField = new JTextField();
         textField.setBounds(20, 25, 300, 125);
         textField.setHorizontalAlignment(JTextField.RIGHT);
         textField.setEditable(false);
         textField.setFont(textFieldFont);
+
+        // create a panel
+        panel = new JPanel();
+        panel.setBounds(20, 175, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(backgroundColor);
+
+        // customized the number buttons
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = new JButton(String.valueOf(i));
+            numbers[i].addActionListener(this);
+            numbers[i].setFont(buttonFont);
+            numbers[i].setFocusable(false);
+            numbers[i].setBackground(Color.WHITE);
+        }
 
         add = new JButton("+");
         sub = new JButton("-");
@@ -48,6 +64,7 @@ public class calculator implements ActionListener {
         methods[4] = equ;
         methods[5] = del;
 
+        // customized the fuction buttons
         for (int i = 0; i < methods.length; i++) {
             methods[i].addActionListener(this);
             methods[i].setFont(buttonFont);
@@ -55,19 +72,7 @@ public class calculator implements ActionListener {
             methods[i].setBackground(methodColor);
         }
 
-        for (int i = 0; i < 10; i++) {
-            numbers[i] = new JButton(String.valueOf(i));
-            numbers[i].addActionListener(this);
-            numbers[i].setFont(buttonFont);
-            numbers[i].setFocusable(false);
-            numbers[i].setBackground(Color.WHITE);
-        }
-
-        panel = new JPanel();
-        panel.setBounds(20, 175, 300, 300);
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
-        panel.setBackground(backgroundColor);
-
+        // add the buttons to the panel
         panel.add(numbers[1]);
         panel.add(numbers[2]);
         panel.add(numbers[3]);
@@ -85,11 +90,13 @@ public class calculator implements ActionListener {
         panel.add(mul);
         panel.add(div);
 
+        // make everything visible to the frame
         frame.add(panel);
         frame.add(textField);
         frame.setVisible(true);
     }
 
+    // do certain action when function buttons were clicked
     @Override
     public void actionPerformed(ActionEvent e) {
         for (int i = 0; i < 10; i++) {
